b6cce8c955c8e9dd6663c4cbca83b090c789913a
Justin <jburton6@aum.edu>
2023-07-21 12:16:27 -0500

    results and most formatting complete. Part of index.html is still displaying when results are shown.

--- static/script.js
+++ static/script.js
@@ -1,139 +1,122 @@
-const selectionForm = document.getElementById('search-form');
-const loadingScreen = document.getElementById('loading-screen');
-const resultContainer = document.getElementById('result-container');
-const blockListContainer = document.getElementById('block-list-container');
-const resultText = document.getElementById('result-text');
-const blockList = document.getElementById('block-list');
+//const selectionForm = document.getElementById('search-form');
+//const loadingScreen = document.getElementById('loading-screen');
+//const resultContainer = document.getElementById('result-container');
+//const resultText = document.getElementById('result-text');
+//const indexContainer = document.getElementById('index-container');
+//const selectionForm = document.getElementById('search-form');
+//const loadingScreen = document.getElementById('loading-container');
+//const resultContainer = document.getElementById('result-container');
+//const blockListContainer = document.getElementById('block-list-container');
+//const resultText = document.getElementById('result-text');
+//const blockList = document.getElementById('block-list');
+//const indexContainer = document.getElementById('index-container');
+//const formContainer = document.getElementById('form-container');
+document.addEventListener('DOMContentLoaded', function() {
+    const selectionForm = document.getElementById('search-form');
+    const loadingScreen = document.getElementById('loading-screen');
+    const resultContainer = document.getElementById('result-container');
+    const resultText = document.getElementById('result-text');
+    const indexContainer = document.getElementById('index-container');
+    const formContainer = document.getElementById('form-container');
 
-// Function to show the loading screen
-function showLoadingScreen() {
-    loadingScreen.style.display = 'block';
-}
+    // Function to show the loading screen
+    function showLoadingScreen() {
+        loadingScreen.style.display = 'block';
+        indexContainer.style.display = 'none';
+    }
 
-// Function to hide the loading screen
-function hideLoadingScreen() {
-    loadingScreen.style.display = 'none';
-}
+    // Function to hide the loading screen
+    function hideLoadingScreen() {
+        loadingScreen.style.display = 'none';
+        indexContainer.style.display = 'block';
+    }
 
-// Function to show the result container
-function showResultContainer() {
-    resultContainer.style.display = 'block';
-}
+    // Function to show the result container
+    function showResultContainer() {
+        resultContainer.style.display = 'block';
+        indexContainer.style.display = 'none';
+    }
 
-// Function to show the block list container
-function showBlockListContainer() {
-    blockListContainer.style.display = 'block';
-}
+    // Function to hide the result container
+    function hideResultContainer() {
+        resultContainer.style.display = 'none';
+        indexContainer.style.display = 'block';
+    }
 
-// Function to hide the result container
-function hideResultContainer() {
-    resultContainer.style.display = 'none';
-}
-
-// Function to hide the block list container
-function hideBlockListContainer() {
-    blockListContainer.style.display = 'none';
-}
-
-// Function to display the blocklist data
-function showBlockList(data) {
-    resultText.innerHTML = ''; // Clear the previous result text
-
-    // Create a new div to hold the block list items
-    const blockListDiv = document.createElement('div');
-
-    data.forEach(item => {
-        const blockItem = document.createElement('p');
-        // Extract the date part from the timestamp (index 0 to 10)
-        const datePart = item.timestamp.slice(4, 12);
-        blockItem.textContent = `Handle: ${item.handle}, Date: ${datePart}`;
-        blockListDiv.appendChild(blockItem);
-    });
-
-    // Append the block list container to the result text
-    resultText.appendChild(blockListDiv);
-
-    // Show the result container
-    resultContainer.style.display = 'block';
-}
-
-// Function to display the total user count
-function showTotalUsersCount(data) {
-    // Clear the previous result text
-    resultText.innerHTML = '';
-
-    const resultParagraph = document.createElement('p');
-    resultParagraph.textContent = `Total Users Count: ${data.count}`;
-
-    // Append the result paragraph to the result text
-    resultText.appendChild(resultParagraph);
-
-    // Show the result container
-    resultContainer.style.display = 'block';
-}
-
-
-// Function to display the result data
-function showResult(data) {
-    // Check if the selection is for "Get Total Users Count" (option 4)
-    if (selection.value === '4') {
-        // For the "Total Users Count" option, show the result in total_users.html
-        showTotalUsersCount(data);
-    } else {
-        // For other selections (options 1, 2, 3, 5), update the result text as before
+    // Function to display the result data
+    function showResult(data) {
         resultText.innerHTML = ''; // Clear the previous result text
-
-        const resultContent = document.createElement('div');
 
         if (data.result) {
             const resultParagraph = document.createElement('p');
             resultParagraph.textContent = data.result;
-            resultContent.appendChild(resultParagraph);
+            resultText.appendChild(resultParagraph);
+        } else if (data.block_list) {
+            const blockListDiv = document.createElement('div');
+            data.block_list.forEach(item => {
+                const blockItem = document.createElement('p');
+                blockItem.textContent = `Handle: ${item.handle}, Date: ${item.timestamp}`;
+                blockListDiv.appendChild(blockItem);
+            });
+            resultText.appendChild(blockListDiv);
         } else {
             const noResultParagraph = document.createElement('p');
             noResultParagraph.textContent = 'No result found.';
-            resultContent.appendChild(noResultParagraph);
+            resultText.appendChild(noResultParagraph);
         }
 
-        // Append the result content to the result text
-        resultText.appendChild(resultContent);
-
         // Show the result container
-        resultContainer.style.display = 'block';
+        showResultContainer();
     }
-}
 
-// Add event listener to the form submit button
-selectionForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the default form submission
-    showLoadingScreen(); // Show the loading screen
-
-    // Perform your form submission or AJAX request using JavaScript Fetch API or Axios
-    fetch('/selection_handle', {
-        method: 'POST',
-        body: new FormData(selectionForm)
-    })
-    .then(response => {
-        // Check if the response status is successful (HTTP 200-299)
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        // Return the response data directly (it's already parsed JSON)
-        return response.json();
-    })
-    .then(data => {
-        // Process the JSON data here
-        hideLoadingScreen(); // Hide the loading screen once the results are ready
-
-        // Call the showResult function to display the data based on selection value
-        showResult(data);
-
-        // Show the result container
-        resultContainer.style.display = 'block';
-    })
-    .catch(error => {
-        // Handle any errors here
+    // Function to handle errors and show the index container if needed
+    function handleErrors(error) {
         console.error("Error fetching data:", error);
         hideLoadingScreen(); // Hide the loading screen in case of an error
+        hideResultContainer(); // Hide the result container and show the index container
+    }
+
+    // Add event listener to the form submit button
+    selectionForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the default form submission
+        showLoadingScreen(); // Show the loading screen
+
+        // Perform your form submission or AJAX request using JavaScript Fetch API or Axios
+        fetch('/selection_handle', {
+            method: 'POST',
+            body: new FormData(selectionForm)
+        })
+        .then(response => {
+            // Check if the response status is successful (HTTP 200-299)
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            // Return the response JSON data
+            return response.json();
+        })
+        .then(data => {
+            // Update the resultText container with the server response
+            showResult(data);
+            // Hide the loading screen
+            hideLoadingScreen();
+            indexContainer.style.display = 'none';
+
+        })
+        .catch(error => {
+            // Handle any errors here
+            handleErrors(error); // Call the function to handle errors and show the index container
+        });
+    });
+
+    // Add event listener to the selection dropdown
+    selection.addEventListener('change', function() {
+        if (selection.value === '4') {
+            // If "Get Total Users Count" is selected, disable the identifier field
+            identifier.value = '';
+            identifier.readOnly = true;
+        } else {
+            // Enable the identifier field
+            identifier.readOnly = false;
+        }
     });
 });
