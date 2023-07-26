c9373a6dd78028da0193c3a3df8c8dbe12013440
Justin <jburton6@aum.edu>
2023-07-26 09:26:58 -0500

    Version increment: 1.1.0

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ logger = logging.getLogger()
 
 title_name = "ClearSky"
 os.system("title " + title_name)
-version = "1.0.1"
+version = "1.1.0"
 current_dir = os.getcwd()
 log_version = "ClearSky Version: " + version
 runtime = datetime.now()
