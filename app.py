0fce86c1b73117bb00c3e0f9e02c74db1f88dae1
Justin <jburton6@aum.edu>
2023-07-26 09:55:26 -0500

    added handling for getting block lists.

--- app.py
+++ app.py
@@ -506,7 +506,7 @@ def get_user_block_list(ident):
         return [], [str(datetime.now().date())]
     if retry_count == max_retries:
         logger.warning("Could not get block list for: " + ident)
-        return [], []
+        return ["error"], [str(datetime.now().date())]
 
     # Return the blocked users and created_at timestamps if needed
     return blocked_users, created_dates
