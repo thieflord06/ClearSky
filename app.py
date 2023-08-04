146dee898e0ad937a147ea84735cfa40215ec6e3
Justin <jburton6@aum.edu>
2023-08-04 11:36:29 -0500

    Updated requirements.txt version increment: 1.6.4

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ config = config_helper.read_config()
 
 title_name = "ClearSky"
 os.system("title " + title_name)
-version = "1.1.0"
+version = "1.6.4"
 current_dir = os.getcwd()
 log_version = "ClearSky Version: " + version
 runtime = datetime.now()
