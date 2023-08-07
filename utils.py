0163048b156e334c9c16101dc8669c70112289ed
Justin <jburton6@aum.edu>
2023-08-07 11:10:21 -0500

    fixed same url issue.

--- utils.py
+++ utils.py
@@ -25,6 +25,7 @@ async def resolve_top_block_lists():
             resolved_blocked.append({'Handle': resolved_did, 'block_count': result['block_count'], 'ProfileURL': f'https://bsky.app/profile/{did}'})
 
     for result in database_handler.blockers_results:
+        did = result['user_did']
         resolved_did = await on_wire.resolve_did(result['user_did'])  # Replace with your actual resolving function
         if resolved_did is not None:
             resolved_blockers.append({'Handle': resolved_did, 'block_count': result['block_count'], 'ProfileURL': f'https://bsky.app/profile/{did}'})
