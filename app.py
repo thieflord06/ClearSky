d0466573102b2da24e13db956a3d0a8f586d73c5
Justin <jburton6@aum.edu>
2023-08-07 10:28:13 -0500

    prep for push to master:
    version increment: 2.0.0
    .ini for prod db

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ config = config_helper.read_config()
 
 title_name = "ClearSky"
 os.system("title " + title_name)
-version = "1.9.8"
+version = "2.0.0"
 current_dir = os.getcwd()
 log_version = "ClearSky Version: " + version
 runtime = datetime.now()
