8202e21cafebd275e594872c33bc11a5f2cdcf9f
Jay <jburton6@aum.edu>
2023-08-30 19:17:33 -0500

    version increment: 3.0.2

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ config = config_helper.read_config()
 
 title_name = "ClearSky"
 os.system("title " + title_name)
-version = "3.0.1"
+version = "3.0.2"
 current_dir = os.getcwd()
 log_version = "ClearSky Version: " + version
 runtime = datetime.now()
