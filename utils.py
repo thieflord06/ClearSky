d5072bfa67c74aa43a1fcbea09e6296d7e55500c
Jay <jburton6@aum.edu>
2023-09-05 09:35:28 -0500

    refactor

--- utils.py
+++ utils.py
@@ -21,7 +21,6 @@ resolved_24blockers_cache = TTLCache(maxsize=100, ttl=3600)
 
 # ======================================================================================================================
 # ============================================= Features functions =====================================================
-
 async def resolve_did(did, count):
     resolved_did = await on_wire.resolve_did(did)
     if resolved_did is not None:
