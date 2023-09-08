a57149042c8a83eb9545db155d07fd91060ba460
Jay <jburton6@aum.edu>
2023-09-08 09:36:08 -0500

    fix if users profile doesn't have a profile picture // version increment: 3.5.1

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ config = config_helper.read_config()
 
 title_name = "ClearSky"
 os.system("title " + title_name)
-version = "3.5.0"
+version = "3.5.1"
 current_dir = os.getcwd()
 log_version = "ClearSky Version: " + version
 runtime = datetime.now()
