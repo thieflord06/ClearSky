77ac990abf35dfb6d70924f71e99cce12159307a
Jay <jburton6@aum.edu>
2023-09-17 19:50:08 -0500

    fixing

---
+++ static/top_block.js
@@ -0,0 +1,29 @@
+// loading.js
+
+// Function to show the loading screen
+function showLoadingScreen() {
+    document.getElementById('loading-screen').style.display = 'block';
+}
+
+// Function to hide the loading screen
+function hideLoadingScreen() {
+    document.getElementById('loading-screen').style.display = 'none';
+}
+
+// Attach an event listener to the "Block Statistics" link
+document.querySelector('a[href="/fun_facts"]').addEventListener('click', function (event) {
+    // Prevent the link from navigating immediately
+    event.preventDefault();
+
+    // Show the loading screen
+    showLoadingScreen();
+
+    // Simulate an asynchronous operation (e.g., fetching data)
+    setTimeout(function () {
+        // After the operation is complete, hide the loading screen
+        hideLoadingScreen();
+
+        // Navigate to the "fun facts" page
+        window.location.href = '/fun_facts';
+    }, 2000); // Adjust the delay as needed for your use case
+});
