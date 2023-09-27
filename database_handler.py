04bdcdb0a98f40617951e643fd8331dcde96091a
Jay <jburton6@aum.edu>
2023-09-26 20:08:16 -0500

    update insert users logic.

--- database_handler.py
+++ database_handler.py
@@ -374,9 +374,10 @@ async def get_all_users_db(run_update=False, get_dids=False, get_count=False, in
                 async with connection.transaction():
                     # Insert data in batches
                     for i in range(0, len(records), batch_size):
-                        batch_data = records[i: i + batch_size]
+                        # batch_data = records[i: i + batch_size]
                         try:
-                            await connection.executemany('INSERT INTO users (did, status) VALUES ($1, TRUE) ON CONFLICT (did) DO UPDATE SET status = TRUE WHERE users.status <> TRUE', batch_data)
+                            for did in records[i: i + batch_size]:
+                                await connection.execute('INSERT INTO users (did, status) VALUES ($1, TRUE) ON CONFLICT (did) DO UPDATE SET status = TRUE WHERE users.status <> TRUE', did)
 
                             logger.info(f"Inserted batch {i // batch_size + 1} of {len(records) // batch_size + 1} batches.")
                         except Exception as e:
