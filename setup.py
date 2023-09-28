56beeee699588b041068eee18f01b5172109820f
Jay <jburton6@aum.edu>
2023-09-27 20:33:19 -0500

    streamline creation

--- setup.py
+++ setup.py
@@ -38,72 +38,37 @@ async def create_db():
                     list_type text
                 )
                 """
-                await connection.execute(create_users_table)
-                await connection.execute(create_blocklists_table)
-                await connection.execute(create_top_blocks_table)
-                await connection.execute(create_top_24_blocks_table)
+                if database_handler.local_db(check_local=True):
+                    logger.info("Creating local db and tables.")
+
+                    conn = database_handler.connection_pool
+                    cursor = conn.cursor()
+
+                    # Execute the SQL statements to create tables
+                    cursor.execute(create_users_table)
+                    cursor.execute(create_blocklists_table)
+                    cursor.execute(create_top_blocks_table)
+                    cursor.execute(create_top_24_blocks_table)
+
+                    # Commit changes and close the connection
+                    conn.commit()
+                    conn.close()
+                else:
+                    logger.info("Creating tables.")
+
+                    await connection.execute(create_users_table)
+                    await connection.execute(create_blocklists_table)
+                    await connection.execute(create_top_blocks_table)
+                    await connection.execute(create_top_24_blocks_table)
     except Exception as e:
         logger.error(f"Error creating db: {e}")
-
-
-def create_local_db():
-    conn = database_handler.connection_pool
-    cursor = conn.cursor()
-
-    create_users_table = """
-    CREATE TABLE IF NOT EXISTS users (
-        did text primary key,
-        handle text,
-        status bool
-    )
-    """
-
-    create_blocklists_table = """
-    CREATE TABLE IF NOT EXISTS blocklists (
-        user_did text,
-        blocked_did text,
-        block_date text
-    )
-    """
-
-    create_top_blocks_table = """
-    CREATE TABLE IF NOT EXISTS top_block (
-        did text,
-        count int,
-        list_type text
-    )
-    """
-
-    create_top_24_blocks_table = """
-    CREATE TABLE IF NOT EXISTS top_twentyfour_hour_block (
-        did text,
-        count int,
-        list_type text
-    )
-    """
-
-    # Execute the SQL statements to create tables
-    cursor.execute(create_users_table)
-    cursor.execute(create_blocklists_table)
-    cursor.execute(create_top_blocks_table)
-    cursor.execute(create_top_24_blocks_table)
-
-    # Commit changes and close the connection
-    conn.commit()
-    conn.close()
 
 
 # ======================================================================================================================
 # =============================================== Main Logic ===========================================================
 async def main():
     await database_handler.create_connection_pool()
-
-    if database_handler.local_db(check_local=True):
-        logger.info("Creating local db and tables.")
-        create_local_db()
-    else:
-        logger.info("creating db tables.")
-        await create_db()
+    await create_db()
 
 
 if __name__ == '__main__':
