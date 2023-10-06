15aa6b74b74091fd9a752e74d6d12c0e6d9a2ee8
Jay <jburton6@aum.edu>
2023-10-06 10:22:30 -0500

    refactor

--- app.py
+++ app.py
@@ -648,8 +648,8 @@ async def autocomplete():
 
         return jsonify({"suggestions": matching_handles})
     else:
-        matching_handles = await database_handler.retrieve_autocomplete_handles(query_without_at)
-        # matching_handles = await database_handler.find_handles(query_without_at)
+        matching_handles = await database_handler.retrieve_autocomplete_handles(query_without_at)  # Use redis
+        # matching_handles = await database_handler.find_handles(query_without_at)  # Only use db
 
         if not matching_handles:
             matching_handles = None
