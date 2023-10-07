2fb242127eba4a241c35d83eca893f747285fbac
Jay <jburton6@aum.edu>
2023-10-07 10:15:00 -0500

    verrsion incerement: 3.9.15

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ config = config_helper.read_config()
 
 title_name = "ClearSky"
 os.system("title " + title_name)
-version = "3.9.14"
+version = "3.9.15"
 current_dir = os.getcwd()
 log_version = "ClearSky Version: " + version
 runtime = datetime.now()
