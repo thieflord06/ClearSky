bbee24484b2322a93b7305086fc4985bf2d3fb10
Jay <jburton6@aum.edu>
2023-10-10 09:28:01 -0500

    add logging.

--- test.py
+++ test.py
@@ -30,6 +30,8 @@ async def generate_random_user_data():
                     user_data
                 )
 
+                logger.info("user data created.")
+
                 return user_data
     except Exception as e:
         logger.error(f"Error generating random data: {e}")
@@ -58,6 +60,8 @@ async def generate_random_block_data(user_data):
                     blocklists_data
                 )
 
+                logger.info("blocklist data created.")
+
     except Exception as e:
         logger.error(f"Error generating random data: {e}")
 
@@ -66,6 +70,6 @@ async def generate_random_block_data(user_data):
 def generate_random_date():
     start_date = datetime.date(2022, 1, 1)  # Change this to your desired start date
     end_date = datetime.date(2023, 9, 15)  # Change this to your desired end date
-
     random_date = start_date + datetime.timedelta(days=random.randint(0, (end_date - start_date).days))
+
     return random_date.isoformat()
