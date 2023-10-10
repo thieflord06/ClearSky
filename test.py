5476bfe45fedcaa10b38a7a670bbfffe61675f39
Jay <jburton6@aum.edu>
2023-10-10 16:28:18 -0500

    add current dynamic date for test data.

--- test.py
+++ test.py
@@ -69,8 +69,9 @@ async def generate_random_block_data(user_data):
 
 # Function to generate a random date within a specified range
 def generate_random_date():
+    current_date = datetime.now().date()
     start_date = datetime.date(2022, 1, 1)  # Change this to your desired start date
-    end_date = datetime.date(2023, 9, 15)  # Change this to your desired end date
+    end_date = current_date  # Change this to your desired end date
     random_date = start_date + datetime.timedelta(days=random.randint(0, (end_date - start_date).days))
 
     return random_date.isoformat()
