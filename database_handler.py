563c2560e0d6785b55b327ff582c84eeac1b0160
Jay <jburton6@aum.edu>
2023-10-12 16:24:10 -0500

    switch no blocks updated logging to debug // blocks updated logging to info

--- database_handler.py
+++ database_handler.py
@@ -405,9 +405,9 @@ async def update_blocklists_batch(batch_dids):
                 # Update the blocklists table in the database with the retrieved data
                 await update_blocklist_table(did, blocked_users, block_dates)
                 total_blocks_updated += 1  # Increment the counter for updated block lists
-                logger.debug(f"Updated block list for DID: {did}")
+                logger.info(f"Updated block list for DID: {did}")
             else:
-                logger.info(f"didn't update no blocks: {did}")
+                logger.debug(f"didn't update no blocks: {did}")
                 continue
         except Exception as e:
             logger.error(f"Error updating block list for DID {did}: {e}")
