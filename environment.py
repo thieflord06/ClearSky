d2d9b38b1671ae4ee253ad7fe40c9b876ed0a1d4
Jay <jburton6@aum.edu>
2023-11-01 14:53:25 -0500

    update default api name

--- environment.py
+++ environment.py
@@ -6,8 +6,8 @@ import os
 if not os.getenv('CLEAR_SKY'):
     api_environment = config.get("environment", "api")
     if not api_environment:
-        api_environment = "production"
+        api_environment = "prod"
 else:
     api_environment = os.environ.get("CLEARSKY_ENVIRONMENT")
     if not api_environment:
-        api_environment = "production"
+        api_environment = "prod"
