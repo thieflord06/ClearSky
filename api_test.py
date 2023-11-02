2f2c0a0e9a730685ff5399463d8d7704456ef4ea
Jay <jburton6@aum.edu>
2023-11-02 13:14:04 -0500

    add api_test.py

---
+++ api_test.py
@@ -0,0 +1,35 @@
+# api_test.py
+
+from config_helper import logger
+import requests
+
+
+def main():
+    api_key = "CLEARSKYtest121ascmkdneaorSDno32"
+
+    api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/base/internal/status/process-status"
+    # api_endpoint = "http://localhost/api/v1/base/internal/status/process-status"
+    # api_endpoint = "http://localhost/api/v1/blocklist-search-blocking/test.tennis.thieflord.dev/alechiaval.bsky.social"
+
+    # Define the headers with the API key
+    headers = {'X-API-Key': f'{api_key}'}
+    try:
+        # Send a GET request to the API endpoint with the headers
+        response = requests.get(api_endpoint, headers=headers)
+
+        # Print the response headers
+        for key, value in response.headers.items():
+            logger.info(f"Header: {key} = {value}")
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Print the response content
+            logger.info(response.text)
+        else:
+            logger.info(f'Request failed with status code {response.status_code}')
+    except requests.exceptions.RequestException as e:
+        logger.info(f'Error: {e}')
+
+
+if __name__ == '__main__':
+    main()
