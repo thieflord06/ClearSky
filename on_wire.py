edae635c3b72d6bf9fe1c206894fb0f0bdc996fa
Jay <jburton6@aum.edu>
2023-11-16 10:31:41 -0600

    add did not found on handle update.

--- on_wire.py
+++ on_wire.py
@@ -86,6 +86,10 @@ async def resolve_did(did):  # Take DID and get handle
                 elif response.status_code == 429:
                     logger.warning("Too many requests, pausing.")
                     await asyncio.sleep(60)
+                elif response.status_code == 404:
+                    logger.warning("404 not found.")
+
+                    return None
                 else:
                     error_message = response_json.get("message", "")
                     logger.debug(error_message)
