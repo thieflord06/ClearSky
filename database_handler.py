c9015bc1a55c2d95fd531d2f55ba1ae59cf5bc14
Jay <jburton6@aum.edu>
2023-11-20 16:55:47 -0600

    use set in deactivate query.

--- database_handler.py
+++ database_handler.py
@@ -555,7 +555,7 @@ async def get_all_users_db(run_update=False, get_dids=False, get_count=False, in
 
         if dids_deactivated:
             logger.info("deactivating dids")
-            await connection.execute('UPDATE users SET status = FALSE WHERE did = ANY($1)', list(dids_deactivated))
+            await connection.execute('UPDATE users SET status = FALSE WHERE did = ANY($1)', dids_deactivated)
             logger.info(f"{str(len(dids_deactivated))} dids deactivated.")
 
         # Return the records when run_update is false and get_count is called
