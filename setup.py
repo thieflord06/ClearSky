a6116263bc8c050a21db62d2f986b5383fad924d
Jay <jburton6@aum.edu>
2023-11-25 21:58:02 -0600

    add users transaction table

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ mute_lists_users_transaction_table = "mutelists_users_transaction"
 subscribe_block_list_table = "subscribe_blocklists"
 subscribe_block_list_transaction_table = "subscribe_blocklists_transaction"
 pds_table = "pds"
+users_transaction_table = "users_transaction"
 
 
 async def create_db():
@@ -174,6 +175,16 @@ async def create_db():
                 create_last_created_table = """CREATE TABLE IF NOT EXISTS {} (
                 last_created timestamptz PRIMARY KEY""".format(last_created_table)
 
+                create_users_transaction_table = """
+                create table users_transaction (
+                    serial_id BIGSERIAL primary key,
+                    did text,
+                    handle text,
+                    timestamp timestamptz,
+                    touched timestamptz,
+                    touched_actor text
+                )""".format(users_transaction_table)
+
                 index_1 = """CREATE INDEX IF NOT EXISTS blocklist_user_did ON blocklists (user_did)"""
                 index_2 = """CREATE INDEX IF NOT EXISTS blocklist_blocked_did ON blocklists (blocked_did)"""
                 index_3 = """CREATE INDEX idx_user_prefixes_prefix1 ON user_prefixes(prefix1)"""
@@ -194,6 +205,7 @@ async def create_db():
                 await connection.execute(create_subscribe_block_list_table)
                 await connection.execute(create_subscribe_block_list_transaction_table)
                 await connection.execute(create_pds_table)
+                await connection.execute(create_users_transaction_table)
 
                 await connection.execute(index_1)
                 await connection.execute(index_2)
