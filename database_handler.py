25917952122bcd28f46278ff8ccad23e80b64123
Jay <jburton6@aum.edu>
2023-12-04 21:00:03 -0600

    change get top block lists to write db.

--- database_handler.py
+++ database_handler.py
@@ -1029,7 +1029,7 @@ async def update_top_block_list_table(entries, list_type):
 
 async def get_top_blocks_list():
     try:
-        async with connection_pools["read"].acquire() as connection:
+        async with connection_pools["write"].acquire() as connection:
             async with connection.transaction():
                 query1 = "SELECT distinct did, count FROM top_block WHERE list_type = 'blocked'"
                 query2 = "SELECT distinct did, count FROM top_block WHERE list_type = 'blocker'"
@@ -1045,7 +1045,7 @@ async def get_top_blocks_list():
 
 async def get_24_hour_block_list():
     try:
-        async with connection_pools["read"].acquire() as connection:
+        async with connection_pools["write"].acquire() as connection:
             async with connection.transaction():
                 query1 = "SELECT distinct did, count FROM top_twentyfour_hour_block WHERE list_type = 'blocked'"
                 query2 = "SELECT distinct did, count FROM top_twentyfour_hour_block WHERE list_type = 'blocker'"
