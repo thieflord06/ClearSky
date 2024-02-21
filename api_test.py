3eb9f2f895c3a2c83ec0cee3bc3fb31a9f6cd4a4
Jay <jburton6@aum.edu>
2024-02-20 20:02:19 -0600

    update api_test.py

--- api_test.py
+++ api_test.py
@@ -11,7 +11,7 @@ def main():
     # api_endpoint = "http://localhost/api/v1/base/internal/status/process-status"
     # api_endpoint = "http://staging.bsky.thieflord.dev/api/v1/blocklist-search-blocking/test.tennis.thieflord.dev"
     # api_endpoint = "http://localhost/api/v1/lists/block-stats"
-    # api_endpoint = "http://staging.bsky.thieflord.dev/api/v1/lists/fun-facts"
+    api_endpoint = "http://api.staging.clearsky.services/api/v1/lists/fun-facts"
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/blocklist-search-blocking/test.tennis.thieflord.dev/alechiaval.bsky.social"
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/lists/block-stats"
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/blocklist/boykisser.expert"
@@ -19,7 +19,7 @@ def main():
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/total-users"
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/get-did/thieflord.dev"
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/get-handle/thieflford.dev"
-    # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/get-handle-history/thieflofgrd.dev"
+    # api_endpoint = "http://localhost/api/v1/get-handle-history/pfrazee.com"
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/blocklist-search-blocking/desir.ee/thieflord.dev"
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/in-common-blocklist/"
     # api_endpoint = "http://localhost/api/v1/blocklist-search-blocking/desir.ee/thieflord.dev"
@@ -27,7 +27,7 @@ def main():
     # api_endpoint = "https://staging.bsky.thieflord.dev/api/v1/total-users"
     # api_endpoint = "http://localhost/api/v1/single-blocklist/thieflord.dev"
     # api_endpoint = "http://localhost/api/v1/at-uri/at://did:plc:smcanwhzsj5dqp4yew7y6ybx/app.bsky.graph.listblock/3kf2kfcic5od2u"
-    api_endpoint = "http://localhost/api/v1/get-moderation-list/gaminsdfvf"
+    # api_endpoint = "http://localhost/api/v1/get-moderation-list/gaminsdfvf"
 
     # Define the headers with the API key
     headers = {'X-API-Key': f'{api_key}'}
