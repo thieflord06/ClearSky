082eb9b081f852d0f1ddd59db05f296c2a118d37
Jay <jburton6@aum.edu>
2024-02-23 09:58:48 -0600

    add update PDS table with new PDSs to update_users_dids process.

--- database_handler.py
+++ database_handler.py
@@ -326,6 +326,19 @@ async def get_dids_without_handles():
 
 
 async def get_pdses():
+    # update PDS table with unique PDSes from users table
+    try:
+        async with connection_pools["write"].acquire() as connection:
+            async with connection.transaction():
+                query = """INSERT INTO pds (pds) 
+                            SELECT DISTINCT pds
+                            FROM users
+                            WHERE pds IS NOT NULL
+                            ON CONFLICT (pds) DO NOTHING"""
+                await connection.execute(query)
+    except Exception as e:
+        logger.error(f"Error inserting PDSes: {e}")
+
     try:
         async with connection_pools["write"].acquire() as connection:
             async with connection.transaction():
