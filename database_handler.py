4f0cec5669d87dbc323403b581a23ca7c1f5cc1a
Jay <jburton6@aum.edu>
2024-03-20 08:54:10 -0500

    add none type error check for pds.

--- database_handler.py
+++ database_handler.py
@@ -750,6 +750,8 @@ async def crawler_batch(batch_dids, forced=False):
     max_retries = 3
 
     for did, pds in batch_dids:
+        if pds is None:
+            continue
         if "bsky.network" not in pds:
             continue
         handle = await on_wire.resolve_did(did)
