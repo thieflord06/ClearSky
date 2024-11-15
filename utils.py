b8423d82db11d19ab1dadc1d5a1baad17b6295c3
Jay <jburton6@aum.edu>
2024-11-15 16:02:40 -0600

    re-enable

--- utils.py
+++ utils.py
@@ -1514,33 +1514,33 @@ async def get_resolution_queue_info():
     batch_number = 0
 
     while True:
-        # all_dids = await database_handler.get_dids_without_handles()
-        #
-        # if all_dids:
-        #     logger.info("Updating dids with no handles.")
-        #
-        #     total_dids = len(all_dids)
-        #
-        #     async with database_handler.connection_pools["write"].acquire() as connection:
-        #         async with connection.transaction():
-        #             # Concurrently process batches and update the handles
-        #             for i in range(0, total_dids, batch_size):
-        #                 logger.info("Getting batch to resolve.")
-        #                 batch_dids = all_dids[i:i + batch_size]
-        #
-        #                 # Process the batch asynchronously
-        #                 batch_handles_updated = await database_handler.process_batch(batch_dids, True, batch_size)
-        #                 total_handles_updated += batch_handles_updated
-        #
-        #                 # Log progress for the current batch
-        #                 logger.info(f"Handles updated: {total_handles_updated}/{total_dids}")
-        #                 logger.debug(f"First few DIDs in the batch: {batch_dids[:5]}")
-        #
-        #                 # Pause after each batch of handles resolved
-        #                 logger.info("Pausing...")
-        #                 await asyncio.sleep(60)  # Pause for 60 seconds
-        #
-        #     total_handles_updated = 0
+        all_dids = await database_handler.get_dids_without_handles()
+
+        if all_dids:
+            logger.info("Updating dids with no handles.")
+
+            total_dids = len(all_dids)
+
+            async with database_handler.connection_pools["write"].acquire() as connection:
+                async with connection.transaction():
+                    # Concurrently process batches and update the handles
+                    for i in range(0, total_dids, batch_size):
+                        logger.info("Getting batch to resolve.")
+                        batch_dids = all_dids[i:i + batch_size]
+
+                        # Process the batch asynchronously
+                        batch_handles_updated = await database_handler.process_batch(batch_dids, True, batch_size)
+                        total_handles_updated += batch_handles_updated
+
+                        # Log progress for the current batch
+                        logger.info(f"Handles updated: {total_handles_updated}/{total_dids}")
+                        logger.debug(f"First few DIDs in the batch: {batch_dids[:5]}")
+
+                        # Pause after each batch of handles resolved
+                        logger.info("Pausing...")
+                        await asyncio.sleep(60)  # Pause for 60 seconds
+
+            total_handles_updated = 0
 
         resolution_queue = await database_handler.get_resolution_queue(batch_size)
 
